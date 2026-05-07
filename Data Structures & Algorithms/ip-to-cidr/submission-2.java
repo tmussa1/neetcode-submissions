d2212bef6b499450ead65596a1725bd166e04e7e
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2026-05-07 16:29:07 +0300

    Add: ip-to-cidr - submission-2

---
+++ Data Structures & Algorithms/ip-to-cidr/submission-2.java
@@ -0,0 +1,75 @@
+class Solution {
+    public List<String> ipToCIDR(String ip, int n) {
+        // 62 111110
+        // 32 + 16 + 8 + 4 + 2 + 0
+        // 63 111111
+        // 64 100000
+        // 68 = 64 + 4 = 1000100
+
+        List<String> result = new ArrayList<>();
+
+        long ipLong = ipToLong(ip);
+
+        if(ipLong == 0L){
+            result.add("0.0.0.0/32");
+            return result;
+        }
+
+        while(n > 0){
+
+
+            long maxBlock = ipLong & (-ipLong);
+
+            while(maxBlock > n){
+                maxBlock >>= 1;
+            }
+
+            long prefix = 32 - Long.numberOfTrailingZeros(maxBlock);
+
+            result.add(longToIp(ipLong) + '/' + prefix);
+
+            ipLong += maxBlock;
+
+            n -= maxBlock;
+        }
+
+        return result;
+    }
+
+    private long ipToLong(String ip){
+
+        long result = 0;
+        String [] ipSplit = ip.split("\\.");
+
+        for(int index = 0; index < ipSplit.length; index++){
+            result = result * 256 + Long.parseLong(ipSplit[index]);
+        }
+
+        return result;
+    }
+
+    private String longToIp(long ip){
+
+        StringBuilder builder = new StringBuilder();
+
+        Stack<String> stack = new Stack<>();
+
+        while(ip > 0){
+            stack.push((ip % 256) + "");
+            ip >>= 8;
+        }
+
+        while(stack.size() < 4){
+            stack.push("0");
+        }
+
+        while(!stack.isEmpty()){
+            if(builder.length() > 0){
+                builder.append(".");
+            }
+            builder.append(stack.pop());
+        }
+
+        return builder.toString();
+    }
+}
