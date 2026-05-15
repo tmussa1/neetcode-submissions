8aa7d274f2378dba9cc9b461a00716303130d93d
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2026-05-15 20:36:38 +0300

    Add: decode-string - submission-1

---
+++ Data Structures & Algorithms/decode-string/submission-1.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String decodeString(String s) {
+        
+        Stack<Integer> numberStack = new Stack<>();
+        Stack<String> characterStack = new Stack<>();
+
+        int index = 0;
+
+        char [] sArr = s.toCharArray();
+
+        while(index < s.length()){
+            char c = sArr[index];
+
+            if(Character.isDigit(c)){
+
+                int beginIndex = index;
+
+                while(Character.isDigit(sArr[index])){
+                    index++;
+                }
+
+                int num = Integer.parseInt(s.substring(beginIndex, index));
+                numberStack.push(num);
+            } else if(c == ']'){
+                int num = numberStack.pop();
+
+                StringBuilder builder = new StringBuilder();
+
+                while(!characterStack.isEmpty() && !characterStack.peek().equals("[")){
+                    builder.append(characterStack.pop());
+                }
+                characterStack.pop();
+                characterStack.push(builder.toString().repeat(num));
+                index++;
+            } else {
+                characterStack.push(c + "");
+                index++;
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        while(!characterStack.isEmpty()){
+            builder.append(characterStack.pop());
+        }
+
+        return builder.reverse().toString();
+    }
+}
