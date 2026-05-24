122b7192a009036eaff7850b7a5ea214284d43dc
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2026-05-24 07:04:46 -0700

    Add: asteroid-collision - submission-8

---
+++ Data Structures & Algorithms/asteroid-collision/submission-8.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int index = 0;
+
+        while(index < asteroids.length){
+
+            int curr = asteroids[index];
+
+            if(curr < 0){
+                 if(!stack.isEmpty() && stack.peek() > 0) {
+                    int toPush = curr;
+                    while(!stack.isEmpty() && stack.peek() > 0){
+                        int currStack = stack.peek();
+                        int diff = currStack + curr;
+                        if(diff > 0){
+                            toPush = Integer.MAX_VALUE;
+                            break;
+                        } else if(diff < 0){
+                            stack.pop();
+                        } else {
+                            stack.pop();
+                            toPush = Integer.MAX_VALUE;
+                            break;
+                        }
+                    }
+
+                    if(toPush != Integer.MAX_VALUE){
+                        stack.push(toPush);
+                    }
+                 } else {
+                    stack.push(curr);
+                }
+            } else {
+                stack.push(curr);
+            }
+
+            index++;
+        }
+
+        int [] result = new int[stack.size()];
+        index = stack.size() - 1;
+
+        while(!stack.isEmpty()){
+            result[index--] = stack.pop();
+        }
+
+        return result;
+    }
+}
