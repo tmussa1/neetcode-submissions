109139bb6bb36b22ef2e5db16963543b2e0308e5
Tofik Mussa <33045155+tmussa1@users.noreply.github.com>
2026-08-31 18:22:58 -0700

    Add: subarray-sums-divisible-by-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sums-divisible-by-k/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+
+
+    public int subarraysDivByK(int[] nums, int k) {
+        
+        Map<Integer, Integer> prefixMap = new HashMap<>();
+
+        int runningSum = 0;
+
+        prefixMap.put(0, 1);
+
+        int result = 0;
+
+        for(int index = 0; index < nums.length; index++){
+
+            runningSum += nums[index];
+
+            int prefix = ((runningSum % k) + k) % k;
+
+            if(prefixMap.containsKey(prefix)){
+                result += prefixMap.get(prefix);
+            }
+
+            prefixMap.put(prefix, prefixMap.getOrDefault(prefix, 0) + 1);
+        }
+
+        return result;
+    }
+}
